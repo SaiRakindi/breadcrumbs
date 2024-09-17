29a6d5f376c7786869e9c07cedaa01dfcf0a1c20
SaiRakindi <sai.rakindi3@gmail.com>
2024-09-17 20:43:10 +0530

    feat: product details and product listing pages

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 const ProductDetails = () => {
-  return <div>ProductDetails</div>;
+  const { id } = useParams();
+  const [product, setProduct] = useState(null);
+
+  useEffect(() => {
+    fetch(`https://dummyjson.com/products/${id}`)
+      .then((res) => res.json())
+      .then((res) => setProduct(res));
+  }, []);
+
+  return (
+    <div>
+      <h3>ProductDetails</h3>
+
+      {product ? (
+        <div style={{ display: "flex" }}>
+          <img src={product.thumbnail} alt={product.title} />
+          <div style={{ marginTop: "30px" }}>
+            <p>$ {product.price}</p>
+            <p>{product.description}</p>
+          </div>
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
 };
 
 export default ProductDetails;
--- src/pages/ProductListing.jsx
+++ src/pages/ProductListing.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const ProductListing = () => {
-  return <div>ProductListing</div>;
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("https:dummyjson.com/products")
+      .then((response) => response.json())
+      .then((res) => {
+        setProducts(res.products);
+      });
+  }, []);
+
+  return (
+    <div>
+      <div className="products-grid">
+        {products.map((product) => (
+          <div className="product-card" key={product.id}>
+            <Link to={`/products/${product.id}`}>
+              <img src={product.thumbnail} alt={product.thumbnail} />
+              <h3>{product.title}</h3>
+              <h3>${product.price}</h3>
+            </Link>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default ProductListing;
