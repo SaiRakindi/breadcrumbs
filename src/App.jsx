33188121d1a2893890f53cc4bf6208fd06265af0
SaiRakindi <sai.rakindi3@gmail.com>
2024-09-17 20:05:27 +0530

    feat: initial setup for breadcrumbs

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
+import ProductDetails from "./pages/ProductDetails";
+import ProductListing from "./pages/ProductListing";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div>
+        {/* breadcrumbs */}
+        {/* Routes */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<ProductListing />} />
+          <Route path="/products/:id" element={<ProductDetails />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
